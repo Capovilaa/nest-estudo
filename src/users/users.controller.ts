cf1a6b9e6a86e0a3476f46d31c415e633ddaed93
Capovila Lucas (CaP/ETS2) <clp3ca@bosch.com>
2024-04-03 11:52:18 -0300

    Services adicionados

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,12 +10,10 @@ import {
 } from '@nestjs/common';
 
 /**
- * @description controllers são as rotas que a aplicação vai chamar pelo front
- * para acessar os métodos HTTP.
+ * @description controllers são as rotas que a aplicação vai chamar pelo front para acessar os métodos HTTP.
  *
- * @dev a ordem em que os métodos são criados pode influenciar no desenvolvimento,
- * pois uma mesma rota pode corresponder para outra. Se tiver uma rota dinâmica, o
- * parâmetro pode acabar sendo qualquer valor, mesmo algum inesperado
+ * @dev a ordem em que os métodos são criados pode influenciar no desenvolvimento, pois uma mesma rota pode corresponder para outra.
+ * Se tiver uma rota dinâmica, o parâmetro pode acabar sendo qualquer valor, mesmo algum inesperado.
  */
 
 @Controller('users')
@@ -23,13 +21,10 @@ export class UsersController {
   /**
   * @description rotas para GET
    
-  * @Params é bem usado para pegar os dados que vêm por parâmetro,
-  * esses que precisam ser tipados .
+  * @Params é bem usado para pegar os dados que vêm por parâmetro, esses que precisam ser tipados.
   
-  * @Query permite passar 'variáveis' pela url, tendo um nome e um valor,
-  sendo reconhecido por '?', essa query precisa ser definida e tipada pelo typescript.
-  Podemos passar ainda um sinal de '?' em sua definição para dizer que não é um campo
-  obrigatório.
+  * @Query permite passar 'variáveis' pela url, tendo um nome e um valor,sendo reconhecido por '?', essa query precisa ser definida e
+  * tipada pelo typescript. Podemos passar ainda um sinal de '?' em sua definição para dizer que não é um campo obrigatório.
   */
 
   // retorna todos os users
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,99 @@
 import { Injectable } from '@nestjs/common';
 
+/**
+ * @description esses services vão ser aplicados nos nossos controllers, aqui que estará contido a lógica
+  
+ * @dev visão geral dos operadores de arrays:
+ * - map : varre array e retorna o mesmo tamanho
+ * - filter : varre array e retorna apenas aos que satisfazem o filtro
+ * - find : retorna o primeiro elemento que satisfaz o filtro, caso não encontre retorna undefined
+ */
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    {
+      id: 1,
+      name: 'Lucas',
+      email: 'lucas@gmail.com',
+      role: 'ADMIN',
+    },
+    {
+      id: 2,
+      name: 'Alex',
+      email: 'alex@gmail.com',
+      role: 'INTERN',
+    },
+    {
+      id: 3,
+      name: 'Tejero',
+      email: 'tejero@gmail.com',
+      role: 'ENGINEER',
+    },
+  ];
+
+  // encontra todos usuários que possuem a determinada role
+  findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    if (role) {
+      return this.users.filter((user) => user.role === role);
+    }
+    return this.users;
+  }
+
+  // encontra o único usuário que possui aquele id
+  findOne(id: number) {
+    const user = this.users.find((user) => user.id === id);
+    return user;
+  }
+
+  // cria novo usuário
+  create(user: {
+    name: string;
+    email: string;
+    role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+  }) {
+    // copia users e organiza em ordem decrescente
+    const usersByHighestId = [...this.users].sort((a, b) => b.id - a.id);
+
+    // cria novo usuário com base no que foi passado e no novo id
+    const newUser = {
+      id: usersByHighestId[0].id + 1,
+      ...user,
+    };
+
+    // adiciona novo usuário no json de usuários
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  // atualiza usuário de determinado id para o que foi passado
+  update(
+    id: number,
+    updatedUser: {
+      name?: string;
+      email?: string;
+      role?: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    // atualiza os dados do usuário
+    this.users = this.users.map((user) => {
+      // encontra usuário e retorna informações antigas e atualizadas
+      if (user.id === id) {
+        return { ...user, ...updatedUser };
+      }
+
+      // se não encontrar
+      return user;
+    });
+
+    // retorna usuário atualizado
+    return this.findOne(id);
+  }
+
+  // deleta usuário com base no id dele
+  delete(id: number) {
+    const removeduser = this.findOne(id);
+    this.users = this.users.filter((user) => user.id !== id);
+    return removeduser;
+  }
+}
